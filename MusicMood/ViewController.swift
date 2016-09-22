dfa961bcfd57be9c6e71932ea6d1cf15e59c8e65
Ivan Rzhanoi <ivanrzhanoi@owlan048151.sras.sic.shibaura-it.ac.jp>
2016-09-22 15:24:51 +0900

    Initial Commit

---
+++ MusicMood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicMood
+//
+//  Created by Ivan Rzhanoi on 22/09/2016.
+//  Copyright © 2016 Ivan Rzhanoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MusicMoodTests/MusicMoodTests.swift
@@ -0,0 +1,36 @@
+//
+//  MusicMoodTests.swift
+//  MusicMoodTests
+//
+//  Created by Ivan Rzhanoi on 22/09/2016.
+//  Copyright © 2016 Ivan Rzhanoi. All rights reserved.
+//
+
+import XCTest
+@testable import MusicMood
+
+class MusicMoodTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
