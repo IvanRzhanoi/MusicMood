17b9da89be8c0d596e4202b3f99d1376151262f6
Ivan Rzhanoi <ivanrzhanoi@Ivans-MacBook-Air.local>
2016-09-25 20:13:04 +0900

    Configured access to users media library. Added filer by genre. For the moment it is set as "Rock". The code needs formatting and documentation.

--- MusicMood/ViewController.swift
+++ MusicMood/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
-import Foundation
 import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MPMediaPickerControllerDelegate {
     
     //let player = AVPlayer()
     var player = MPMusicPlayerController()
+    var album = MPMediaItemPropertyAlbumTitle
+    //var mood = MPMediaItemPropertyUserGrouping
+    var mood = MPMediaItemPropertyComments
     
-
     
     
     @IBOutlet weak var currentMood: UILabel!
+    
+    @IBAction func pick(_ sender: AnyObject) {
+        // Useless for now
+        /*
+        let picker = MPMediaPickerController()
+        picker.delegate = self
+        picker.allowsPickingMultipleItems = false
+        present(picker, animated: true, completion: nil)
+        */
+    }
+    
     @IBAction func playPause(_ sender: AnyObject) {
+        
         player.play()
         /*if player.playbackState != 1.0 {
             currentMood.text = "rarar"
@@ -39,7 +52,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        player = MPMusicPlayerController.applicationMusicPlayer()
+        player = MPMusicPlayerController.systemMusicPlayer()
+        
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.runMediaLibraryQuery()
+            } else {
+                //self.displayMediaLibraryError()
+                print("Nope")
+            }
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,6 +72,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Shall be implemented later
+    /*
+    func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        player.setQueue(with: mediaItemCollection)
+        player.play()
+    }
+    
+    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    */
+    
+    func runMediaLibraryQuery() {
+        
+        // Get all songs from the library
+        let mediaItems = MPMediaQuery.songs().items
+        // Filter
+        var query = MPMediaQuery.songs()
+        let predicateByMood = MPMediaPropertyPredicate(value: "Rock", forProperty: MPMediaItemPropertyGenre)
+        
+        //query.filterPredicates = NSSet(object: predicateByMood)
+        
+        query.addFilterPredicate(predicateByMood)
+        
+        let mediaCollection = MPMediaItemCollection(items: query.items!)
+        player.setQueue(with: mediaCollection)
+        
+        /*
+        let query = MPMediaQuery.songs()
+        if let items = query.items, let item = items.first {
+            NSLog("Title: \(item.title)")
+        }
+        */
+    }
 }
 
