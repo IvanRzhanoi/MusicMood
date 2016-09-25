8ec516f0b95c21a4fa075b898444631a0f1491e0
Ivan Rzhanoi <ivanrzhanoi@Ivans-MacBook-Air.local>
2016-09-25 14:56:25 +0900

    Play and Pause buttons implemented, but not working properly.

--- MusicMood/ViewController.swift
+++ MusicMood/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Foundation
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    //let player = AVPlayer()
+    var player = MPMusicPlayerController()
+    
 
+    
+    
+    @IBOutlet weak var currentMood: UILabel!
+    @IBAction func playPause(_ sender: AnyObject) {
+        player.play()
+        /*if player.playbackState != 1.0 {
+            currentMood.text = "rarar"
+            player.play()
+        }
+        else {
+            // Playing, so pause.
+            currentMood.text = "Static"
+            player.pause()
+        }*/
+    }
+    @IBAction func next(_ sender: AnyObject) {
+        // For now for testing it is pausing
+        player.pause()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        player = MPMusicPlayerController.applicationMusicPlayer()
     }
 
     override func didReceiveMemoryWarning() {
