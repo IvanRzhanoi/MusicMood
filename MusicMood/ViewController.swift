b8b59ac3e966008ebbde3695f0492c0a4d274ac1
Ivan Rzhanoi <ivanrzhanoi@owlan054102.sras.sic.shibaura-it.ac.jp>
2016-09-26 13:44:42 +0900

    Picker works as well as the play button. Now user can select a favourite genre and play music of that genre. Previous button also allows to choose the previous item. Sadly, it is not possible now to play and pause with the same button. The player can be only started.

--- MusicMood/ViewController.swift
+++ MusicMood/ViewController.swift
@@ -9,31 +9,39 @@
 import UIKit
 import MediaPlayer
 
-class ViewController: UIViewController, MPMediaPickerControllerDelegate {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, MPMediaPickerControllerDelegate {
     
     //let player = AVPlayer()
     var player = MPMusicPlayerController()
     var album = MPMediaItemPropertyAlbumTitle
     //var mood = MPMediaItemPropertyUserGrouping
     var mood = MPMediaItemPropertyComments
-    
+    var currentMoodValue = "Rock"
     
     
     @IBOutlet weak var currentMood: UILabel!
     
     @IBAction func pick(_ sender: AnyObject) {
         // Useless for now
-        /*
+        
         let picker = MPMediaPickerController()
         picker.delegate = self
         picker.allowsPickingMultipleItems = false
         present(picker, animated: true, completion: nil)
-        */
+ 
     }
     
+    @IBAction func genre(_ sender: AnyObject) {
+        genrePicker.isHidden = false
+    }
+    
+    
+    let genrePickerValues = ["Rock", "Pop", "Jazz"]
+    @IBOutlet weak var genrePicker: UIPickerView!
+    
     @IBAction func playPause(_ sender: AnyObject) {
+        genrePicker.isHidden = true
         
-        player.play()
         /*if player.playbackState != 1.0 {
             currentMood.text = "rarar"
             player.play()
@@ -43,15 +51,35 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
             currentMood.text = "Static"
             player.pause()
         }*/
+        
+        runMediaLibraryQuery()
+        player.play()
+        /*
+        if (player.nowPlayingItem != nil) {
+            player.pause()
+        } else {
+            player.play()
+        }
+        */
     }
+    @IBAction func previous(_ sender: AnyObject) {
+        player.skipToPreviousItem()
+    }
+    
     @IBAction func next(_ sender: AnyObject) {
         // For now for testing it is pausing
-        player.pause()
+        player.skipToNextItem()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.genrePicker.dataSource = self
+        self.genrePicker.delegate = self
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        genrePicker.isHidden = true
+        
         player = MPMusicPlayerController.systemMusicPlayer()
         
         MPMediaLibrary.requestAuthorization { (status) in
@@ -73,7 +101,7 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
     }
 
     // Shall be implemented later
-    /*
+    
     func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
         player.setQueue(with: mediaItemCollection)
         player.play()
@@ -82,7 +110,7 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
     func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
         dismiss(animated: true, completion: nil)
     }
-    */
+ 
     
     func runMediaLibraryQuery() {
         
@@ -90,10 +118,11 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         let mediaItems = MPMediaQuery.songs().items
         // Filter
         var query = MPMediaQuery.songs()
-        let predicateByMood = MPMediaPropertyPredicate(value: "Rock", forProperty: MPMediaItemPropertyGenre)
+        let predicateByMood = MPMediaPropertyPredicate(value: currentMoodValue, forProperty: MPMediaItemPropertyGenre)
         
         //query.filterPredicates = NSSet(object: predicateByMood)
         
+        //query.filterPredicates(predicateByMood)
         query.addFilterPredicate(predicateByMood)
         
         let mediaCollection = MPMediaItemCollection(items: query.items!)
@@ -106,5 +135,35 @@ class ViewController: UIViewController, MPMediaPickerControllerDelegate {
         }
         */
     }
+    
+    // UIPickerView
+    func numberOfComponents(in: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return genrePickerValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return genrePickerValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            currentMood.text = genrePickerValues[row]
+            currentMoodValue = genrePickerValues[row]
+        case 1:
+            currentMood.text = genrePickerValues[row]
+            currentMoodValue = genrePickerValues[row]
+        case 2:
+            currentMood.text = genrePickerValues[row]
+            currentMoodValue = genrePickerValues[row]
+        default:
+            currentMood.text = "Static"
+        }
+    }
+
 }
 
