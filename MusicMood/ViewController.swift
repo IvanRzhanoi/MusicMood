76492af0881b3cffa4776798495475a6cfddf51b
Ivan Rzhanoi <ivanrzhanoi@Ivans-MacBook-Air.local>
2016-11-03 15:21:04 +0900

    Added the Navigation Controller, but now there is an error XPC "something-something" Bluetooth doesn't work

--- MusicMood/ViewController.swift
+++ MusicMood/ViewController.swift
@@ -11,6 +11,12 @@ import MediaPlayer
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, MPMediaPickerControllerDelegate {
     
+    // Declaration for Muse
+    var manager = IXNMuseManagerIos();
+    var muse = IXNMuse();
+    //let MuseController = SimpleController()
+    
+    // Declaration for player
     //let player = AVPlayer()
     var player = MPMusicPlayerController()
     var album = MPMediaItemPropertyAlbumTitle
@@ -21,6 +27,24 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     @IBOutlet weak var currentMood: UILabel!
     
+    
+    // MUSE Connectivity
+
+    @IBOutlet weak var alphaCurrent: UILabel!
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Player part
+    let genrePickerValues = ["Rock", "Pop", "Jazz"]
+    @IBOutlet weak var genrePicker: UIPickerView!
     @IBAction func pick(_ sender: AnyObject) {
         // Useless for now
         
@@ -36,8 +60,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     
-    let genrePickerValues = ["Rock", "Pop", "Jazz"]
-    @IBOutlet weak var genrePicker: UIPickerView!
     
     @IBAction func playPause(_ sender: AnyObject) {
         genrePicker.isHidden = true
@@ -78,6 +100,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         
         // Do any additional setup after loading the view, typically from a nib.
+        manager = IXNMuseManagerIos.sharedManager()
+        
+        
+        
+        
+        
+        // Loading things for the player
         genrePicker.isHidden = true
         
         player = MPMusicPlayerController.systemMusicPlayer()
@@ -90,8 +119,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                 print("Nope")
             }
         }
-        
-        
+        //MuseController.receive(IXNMuseConnectionPacket, muse: IXNMuse)
         
     }
 
@@ -100,6 +128,119 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Functions for Muse connectivity
+    
+    func receiveMuseConnectionPacket(packet: IXNMuseConnectionPacket, muse: IXNMuse) {
+        var state: String
+        switch packet.currentConnectionState {
+        case IXNConnectionState.disconnected:
+            state = "disconnected"
+            break
+        case IXNConnectionState.connected:
+            state = "connected"
+            break
+        case IXNConnectionState.connecting:
+            state = "connecting"
+            break
+        case IXNConnectionState.needsUpdate:
+            state = "needs update"
+            break
+        case IXNConnectionState.unknown:
+            state = "unknown"
+            break
+        default:
+            fatalError("impossible connection state received")
+        }
+        print(state)
+    }
+    
+    
+    
+    
+    func connect() {
+        //self.muse.register(self)
+        //self.muse.register(self, type: IXNMuseDataPacketType.artifacts)
+        //self.muse.register(self, type: IXNMuseDataPacketType.alphaAbsolute)
+        self.muse.runAsynchronously()
+    }
+    
+    /*
+    - (void) connect {
+    [self.muse registerConnectionListener:self];
+    [self.muse registerDataListener:self
+    type:IXNMuseDataPacketTypeArtifacts];
+    [self.muse registerDataListener:self
+    type:IXNMuseDataPacketTypeAlphaAbsolute];
+    /*
+     [self.muse registerDataListener:self
+     type:IXNMuseDataPacketTypeEeg];
+     */
+    [self.muse runAsynchronously];
+    }
+    */
+    
+    
+    
+    
+    
+    
+    
+    func receiveMuseDataPacket(packet: IXNMuseDataPacket, muse: IXNMuse) {
+        if packet.packetType() == IXNMuseDataPacketType.alphaAbsolute || packet.packetType() == IXNMuseDataPacketType.eeg {
+            print("Alpha: \(IXNEeg.EEG1.rawValue) Beta: \(IXNEeg.EEG2) Gamma: \(IXNEeg.EEG3) Theta: \(IXNEeg.EEG4)", packet.values())
+            print("%5.2f %5.2f %5.2f %5.2f", packet.values())
+        }
+    }
+    
+    
+    /*
+    - (void)receiveMuseDataPacket:(IXNMuseDataPacket *)packet
+    muse:(IXNMuse *)muse {
+    if (packet.packetType == IXNMuseDataPacketTypeAlphaAbsolute ||
+    packet.packetType == IXNMuseDataPacketTypeEeg) {
+    [self log:@"%5.2f %5.2f %5.2f %5.2f",
+    [packet.values[IXNEegEEG1] doubleValue],
+    [packet.values[IXNEegEEG2] doubleValue],
+    [packet.values[IXNEegEEG3] doubleValue],
+    [packet.values[IXNEegEEG4] doubleValue]];
+    }
+    }
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Functions for the media player
+    
+    
     // Shall be implemented later
     
     func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
